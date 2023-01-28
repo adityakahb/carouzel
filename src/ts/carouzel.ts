670cd6c80dd7f6f20498c56814dc09263f4bb783
adityakahb <aditya.bhakay@live.com>
2023-01-28 22:53:52 +0530

    2.0.0 setup

--- src/ts/carouzel.ts
+++ src/ts/carouzel.ts
@@ -35,15 +35,15 @@ export namespace Carouzel {
 
   interface ISettings {
     activeClass: string;
-    afterInitFn?: () => void;
-    afterScrollFn?: () => void;
+    afterInit?: () => void;
+    afterScroll?: () => void;
     animationEffect: string;
     animationSpeed: number;
     appendUrlHash: boolean;
     autoplay: boolean;
     autoplaySpeed: number;
-    beforeInitFn?: () => void;
-    beforeScrollFn?: () => void;
+    beforeInit?: () => void;
+    beforeScroll?: () => void;
     breakpoints?: IBreakpoint[];
     centerBetween: number;
     disabledClass: string;
@@ -81,7 +81,137 @@ export namespace Carouzel {
     remove: () => void;
   }
 
-  let instances: IInstances = {};
+  interface ICarouzelEasing {
+    [key: string]: (t: number) => number;
+  }
+  /*
+   * Easing Functions - inspired from http://gizma.com/easing/
+   * only considering the t value for the range [0, 1] => [0, 1]
+   */
+  const cEasingFunctions: ICarouzelEasing = {
+    // no easing, no acceleration
+    linear: (t: number) => t,
+    // accelerating from zero velocity
+    easeInQuad: (t: number) => t * t,
+    // decelerating to zero velocity
+    easeOutQuad: (t: number) => t * (2 - t),
+    // acceleration until halfway, then deceleration
+    easeInOutQuad: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+    // accelerating from zero velocity
+    easeInCubic: (t: number) => t * t * t,
+    // decelerating to zero velocity
+    easeOutCubic: (t: number) => --t * t * t + 1,
+    // acceleration until halfway, then deceleration
+    easeInOutCubic: (t: number) =>
+      t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+    // accelerating from zero velocity
+    easeInQuart: (t: number) => t * t * t * t,
+    // decelerating to zero velocity
+    easeOutQuart: (t: number) => 1 - --t * t * t * t,
+    // acceleration until halfway, then deceleration
+    easeInOutQuart: (t: number) =>
+      t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t,
+    // accelerating from zero velocity
+    easeInQuint: (t: number) => t * t * t * t * t,
+    // decelerating to zero velocity
+    easeOutQuint: (t: number) => 1 + --t * t * t * t * t,
+    // acceleration until halfway, then deceleration
+    easeInOutQuint: (t: number) =>
+      t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t
+    // elastic bounce effect at the beginning
+    // easeInElastic: (t: number) => (0.04 - 0.04 / t) * Math.sin(25 * t) + 1,
+    // elastic bounce effect at the end
+    // easeOutElastic: (t: number) => ((0.04 * t) / --t) * Math.sin(25 * t),
+    // elastic bounce effect at the beginning and end
+    // easeInOutElastic: (t: number) =>
+    //   (t -= 0.5) < 0
+    //     ? (0.02 + 0.01 / t) * Math.sin(50 * t)
+    //     : (0.02 - 0.01 / t) * Math.sin(50 * t) + 1,
+  };
+
+  const cAnimationDirections = [`previous`, `next`];
+  const cAnimationEffects = [`scroll`, `slide`, `fade`];
+  const cRootSelectorTypeError = `Element(s) with the provided query do(es) not exist`;
+  const cOptionsParseTypeError = `Unable to parse the options string`;
+  const cDuplicateBreakpointsTypeError = `Duplicate breakpoints found`;
+  const cBreakpointsParseTypeError = `Error parsing breakpoints`;
+  const cNoEffectFoundError = `Animation effect function not found in presets. Try using one from (${cAnimationEffects.join(
+    `, `
+  )}). Setting the animation effect to ${cAnimationEffects[0]}.`;
+  const cNoEasingFoundError = `Easing function not found in presets. Try using one from [${Object.keys(
+    cEasingFunctions
+  ).join(`, `)}]. Setting the easing function to ${
+    Object.keys(cEasingFunctions)[0]
+  }.`;
+  const cUseCapture = false;
+  const cSelectors = {
+    arrowN: `[data-carouzel-nextarrow]`,
+    arrowP: `[data-carouzel-previousarrow]`,
+    cntr: `[data-carouzel-centered]`,
+    ctrlW: `[data-carouzel-ctrlWrapper]`,
+    curp: `[data-carouzel-currentpage]`,
+    dot: `[data-carouzel-dot]`,
+    nav: `[data-carouzel-navigation]`,
+    navW: `[data-carouzel-navigationwrapper]`,
+    opts: `[data-carouzel-options]`,
+    pauseBtn: `[data-carouzel-pause]`,
+    playBtn: `[data-carouzel-play]`,
+    root: `[data-carouzel]`,
+    rtl: `[data-carouzel-rtl]`,
+    scbar: `[data-carouzel-hasscrollbar]`,
+    scbarB: `[data-carouzel-scrollbarthumb]`,
+    scbarT: `[data-carouzel-scrollbartrack]`,
+    scbarW: `[data-carouzel-scrollbarwrapper]`,
+    slide: `[data-carouzel-slide]`,
+    stitle: `[data-carouzel-title]`,
+    totp: `[data-carouzel-totalpages]`,
+    trk: `[data-carouzel-track]`,
+    trkM: `[data-carouzel-trackMask]`,
+    trkO: `[data-carouzel-trackOuter]`,
+    trkW: `[data-carouzel-trackWrapper]`,
+    ver: `[data-carouzel-vertical]`
+  };
+  const cDefaults: ISettings = {
+    activeClass: `__carouzel-active`,
+    animationEffect: cAnimationEffects[0],
+    animationSpeed: 1000,
+    appendUrlHash: false,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    breakpoints: [],
+    centerBetween: 0,
+    disabledClass: `__carouzel-disabled`,
+    dotIndexClass: `__carouzel-pageindex`,
+    dotTitleClass: `__carouzel-pagetitle`,
+    duplicateClass: `__carouzel-duplicate`,
+    easingFunction: `linear`,
+    editModeClass: `__carouzel-editmode`,
+    enableKeyboard: true,
+    // enableScrollbar: false,
+    enableTouchSwipe: true,
+    hiddenClass: `__carouzel-hidden`,
+    idPrefix: `__carouzel`,
+    isInfinite: true,
+    isRtl: false,
+    isVertical: false,
+    nextDirectionClass: `__carouzel-next`,
+    pauseOnHover: false,
+    previousDirectionClass: `__carouzel-previous`,
+    showArrows: true,
+    showNavigation: true,
+    slideGutter: 0,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    startAtIndex: 1,
+    syncWith: ``,
+    touchThreshold: 125,
+    trackUrlHash: false,
+    useTitlesAsDots: false,
+    verticalHeight: 480
+  };
+
+  const allInstances: IInstances = {};
+  let instanceIndex = 0;
 
   class Root {
     constructor(el?: Node, settings?: ISettings) {}
@@ -92,14 +222,46 @@ export namespace Carouzel {
     public destroy() {}
   }
 
+  const generate = (el: Node, options: ISettings) => {
+    const element = el as HTMLElement;
+    let elementId = element.getAttribute(`id`);
+
+    if (!elementId) {
+      elementId = `${
+        { ...cDefaults, ...options }.idPrefix
+      }_${new Date().getTime()}_root_${instanceIndex++}`;
+      element.setAttribute(`id`, elementId);
+    }
+
+    if (!allInstances[elementId]) {
+      allInstances[elementId] = new Root(el);
+    }
+
+    return allInstances[elementId];
+  };
+
   const initGlobal = () => {
-    $$('[data-carouzel]').forEach((el: Node) => {
-      new Root(el);
+    $$(cSelectors.root).forEach((el: Node) => {
+      const element = el as HTMLElement;
+      let newOptions;
+      const optionsDataAttr =
+        element.getAttribute(cSelectors.opts.slice(1, -1)) || ``;
+      if (optionsDataAttr) {
+        try {
+          newOptions = JSON.parse(optionsDataAttr.trim().replace(/'/g, `"`));
+        } catch (e) {
+          // throw new TypeError(cOptionsParseTypeError);
+          console.error(cOptionsParseTypeError);
+        }
+      } else {
+        newOptions = {};
+      }
+      generate(el, newOptions);
     });
   };
 
-  export const init = (el?: Node, settings?: ISettings) => {
-    return new Root(el, settings);
+  export const init = (el: Node, settings?: ISettings) => {
+    return generate(el, settings || ({} as ISettings));
   };
 
   export const getVersion = () => {
@@ -107,7 +269,4 @@ export namespace Carouzel {
   };
 
   initGlobal();
-  console.log('=======instances', instances);
 }
-const temp = Carouzel.init();
-console.log('=======temp', temp);
